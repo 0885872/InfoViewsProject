b0f4d3288771e7b20a8a71ffb4f8cf30a9ba9ab1
0885872 <0885872@hr.nl>
2018-05-25 11:14:16 +0200

    Add project files.

---
+++ InfoViewsProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InfoViewsProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
